67372e7438b2d4d5c6e9349ca790909f61724f0a
韭菜 <891841626@qq.com>
2025-10-26 07:56:42 +0800

    Update API documentation and remove old Omise docs

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Hello World'
+    ]);
+   // return view('welcome');
 });
